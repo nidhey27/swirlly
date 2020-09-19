3c1112d465d862e973a9d25e85b4cc3d12559742
Your Name <you@example.com>
2020-09-19 07:06:21 -0700

    Swirlly Portfolio

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TeamComponent } from './team/team.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path : 'home' , component : HomeComponent},
+  {path : 'team' , component : TeamComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
